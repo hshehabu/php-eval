20f884b37bb375ff0f8c8501fe9a82e408f7b5a8
hshehabu <hamzashehabu457@gmail.com>
2024-11-15 23:42:56 +0300

    [Test 1] :  "Test random number with in range" finished

---
+++ test.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once('utils.php');
+use General\Utils;
+
+/**
+ * let's verify the numbers are in range
+ * 
+ */
+
+ function testRandomNumberWithinRange()
+ {
+    $min = 10;
+    $max = 17;
+
+    echo "Test 1: Numbers within range [$min,$max]\n";
+
+    for ($i = 0 ; $i < 10 ; $i++){
+        $randomNumber = Utils::getSecureRandom($min, $max);
+        if($randomNumber < $min || $randomNumber > $max){
+            echo "Test 1 failed: Random number $randomNumber is not within the range [$min,$max]\n";
+            return;
+        }
+    }
+    echo "Test 1 passed: All numbers are within range\n";
+ }
+ 
+?>
